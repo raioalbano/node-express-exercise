3f256771fad6859271734406f63fdaee9b943709
Raissa <raissa@Students-MacBook-Pro.local>
2018-11-12 13:12:13 -0500

    Dependencies + first celebrity version

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+/*app.get('/', (req, res) =>{
+    res.send(`Node and express are running on port ${PORT}`)
+});*/
+
+
+module.exports = app;
